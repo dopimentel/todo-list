cd7436cb77fe89bbc0b2c01932ce417cc93074b0
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-07-29 18:10:09 -0300

    feat: filter state

--- front-end/src/context/taskContext.js
+++ front-end/src/context/taskContext.js
@@ -4,6 +4,7 @@ const TaskContext = createContext();
 
 export function TaskProvider ({ children }) {
   const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState('all');
 
   const addTask = (description) => {
     const newTask = { description, check: false };
@@ -25,12 +26,28 @@ export function TaskProvider ({ children }) {
     setTasks(updatedTasks);
   }
 
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === 'all') {
+      return true;
+    }
+    if (filter === 'checked') {
+      return task.check;
+    }
+    if (filter === 'unchecked') {
+      return !task.check;
+    }
+    return true;
+  });
+
 
   const contextValue = {
-    tasks,
+    tasks: filteredTasks,
+    filter,
     addTask,
     toggleCheck,
     removeTask,
+    setFilter,
+
   };
 
   return (
