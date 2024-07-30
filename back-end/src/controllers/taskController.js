1b03aeeb3ae41b68dee75e326aae300bc152b5a4
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-07-30 13:11:49 -0300

    refactor: change json response

--- back-end/src/controllers/taskController.js
+++ back-end/src/controllers/taskController.js
@@ -3,7 +3,7 @@ const taskModel = require('../models/taskModel');
 const getAllTasks = async (_req, res) => {
     try {
         const tasks = await taskModel.getAllTasks();
-        res.status(200).json({ data: tasks });
+        res.status(200).json(tasks);
     }
     catch (err) {
         res.status(500).json({ message: err.message });
