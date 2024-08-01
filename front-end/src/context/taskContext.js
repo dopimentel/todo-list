06bfd4e1774e2a5bea8c2b62aa483a398ea55d27
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-08-01 04:05:27 -0300

    refactor: improve performance by reducing unnecessary requests

--- front-end/src/context/taskContext.js
+++ front-end/src/context/taskContext.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import tasksApi from '../utils/fetch';
 
 const SUCCESS_STATUS = 200;
+const NO_CONTENT_STATUS = 204;
 const NOT_FOUND_STATUS = 404;
 
 const TaskContext = createContext();
@@ -46,28 +47,24 @@ export function TaskProvider({ children }) {
     }
   }, [getTasks]);
 
-  const updateTasks = (updatedTask) => {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-    const newTasks = storedTasks.map((task) => {
-      if (task.id === updatedTask.id) {
-        return updatedTask;
-      }
-      return task;
-    });
-    setTasks(newTasks);
-  };
-
-  const toggleCheck = useCallback(async (task) => {
-    const { id, description, check } = task;
-    const updateTask = {
+  const toggleCheck = useCallback(async (t) => {
+    const { id, description, check } = t;
+    const updatedTask = {
       id,
       description,
       check: !check,
     };
     try {
-      const response = await tasksApi('PUT', `/tasks/${id}`, updateTask);
+      const response = await tasksApi('PUT', `/tasks/${id}`, updatedTask);
       if (response.status === SUCCESS_STATUS) {
-        updateTasks(updateTask);
+        const newTasks = tasks.map((task) => {
+          if (task.id === id) {
+            return updatedTask;
+          }
+          return task;
+        });
+        setTasks(newTasks);
+        saveTasks(newTasks);
       }
     } catch (err) {
       console.error(err);
@@ -76,20 +73,25 @@ export function TaskProvider({ children }) {
         getTasks();
       }
     }
-  }, [getTasks]);
+  }, [tasks, getTasks]);
 
   const removeTask = useCallback(async (id) => {
     try {
-      await tasksApi('DELETE', `/tasks/${id}`);
-      await getTasks();
+      const response = await tasksApi('DELETE', `/tasks/${id}`);
+      if (response.status === NO_CONTENT_STATUS) {
+        const newTasks = tasks.filter((task) => task.id !== id);
+        setTasks(newTasks);
+        saveTasks(newTasks);
+      }
     } catch (err) {
       console.error(err);
       if (err.response.status === NOT_FOUND_STATUS) {
         console.log('Task not found');
         getTasks();
+        saveTasks(tasks);
       }
     }
-  }, [getTasks]);
+  }, [tasks, getTasks]);
 
   const saveFilter = (newFilter) => {
     localStorage.setItem('filter', newFilter);
