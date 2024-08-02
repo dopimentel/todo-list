5e1a4f6df9b899838d1bf4e6bc3cef50f4363e8d
Marcos Pimentel <mpc_marcos@hotmail.com>
2024-08-02 01:07:39 -0300

    ops: deploy - setting URL API

--- front-end/src/utils/fetch.js
+++ front-end/src/utils/fetch.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
-const HOST = process.env.REACT_APP_API_HOST || 'localhost';
-const DEFAULT_PORT = 3001;
-const PORT = process.env.REACT_APP_API_PORT || DEFAULT_PORT;
+// const HOST = process.env.REACT_APP_API_HOST || 'localhost';
+// const DEFAULT_PORT = 3001;
+// const PORT = process.env.REACT_APP_API_PORT || DEFAULT_PORT;
+
+const URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 const fetch = axios.create({
-  baseURL: `http://${HOST}:${PORT}`,
+  baseURL: `${URL}`,
   timeout: 3000,
   headers: {
     Accept: 'application/json',
